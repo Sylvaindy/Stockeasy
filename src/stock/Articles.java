9fa2d19dde46e6599cfb35e1b29825fe9af759e8
Sylvaindy <sylvain.diez-ypiens@univ-tlse.fr>
2022-12-08 16:22:35 +0100

    Debut

--- src/stock/Articles.java
+++ src/stock/Articles.java
@@ -1,5 +1,31 @@
 package stock;
 
 public class Articles {
+	private int NbArticles=0;
+	private Articles[] magasin = new Articles[NbArticles];
+	private String nom;
+	private int quantite;
+	
+	public Articles(String nom, int quantite) {
+		this.nom = nom;
+		this.quantite = quantite;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	public int getQuantite() {
+		return quantite;
+	}
+	
+	public boolean estDansMagasin(Articles magasin[], int NbArticles, Articles articles) {
+		for (int i=0;i<NbArticles;i++) {
+			if (magasin[i]==nom) {
+				return true;
+			}
+		}
+	}
 
 }
