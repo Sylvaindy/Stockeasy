791aba73cd4db1fcf612793bc50c9fbff9671d3b
Sylvaindy <sylvain.diez-ypiens@univ-tlse.fr>
2022-12-08 16:59:27 +0100

    Debut methode 2

--- src/stock/Articles.java
+++ src/stock/Articles.java
@@ -20,12 +20,24 @@ public class Articles {
 		return quantite;
 	}
 	
-	public boolean estDansMagasin(Articles magasin[], int NbArticles, Articles articles) {
+	public boolean estDansMagasin(Articles magasin[], int NbArticles, Articles articles) { //Stockage//
+		boolean test = false;
 		for (int i=0;i<NbArticles;i++) {
-			if (magasin[i]==nom) {
-				return true;
+			if (magasin[i].getNom()==articles.getNom()) {
+				test=false;
+				return test;
 			}
 		}
+		return test;
+	}
+	
+	public void Maj(Articles magasin[], Articles articles) {
+		int i = 0;
+		while (magasin[i].getNom()!=articles.getNom()) {
+			i += 1;
+		}
+		int quantite = magasin[i].getQuantite() + articles.getQuantite();
+		  
 	}
 
 }
