93ea6145f5d9bea89f40b7c32bb8d5d19de1b27f
Sylvaindy <sylvain.diez-ypiens@univ-tlse.fr>
2022-12-10 13:42:12 +0100

    Gros des fonctions fait

--- src/stock/Articles.java
+++ src/stock/Articles.java
@@ -3,12 +3,17 @@ package stock;
 public class Articles {
 	private int NbArticles=0;
 	private Articles[] magasin = new Articles[NbArticles];
+	private Articles[] comparaison = new Articles[NbArticles];
 	private String nom;
 	private int quantite;
+	private float prix;
+	private float prixAchat;
 	
-	public Articles(String nom, int quantite) {
+	public Articles(String nom, int quantite, float prix, float prixAchat) {
 		this.nom = nom;
 		this.quantite = quantite;
+		this.prix = prix;
+		this.prixAchat = prixAchat;
 	}
 	
 	public String getNom() {
@@ -20,24 +25,116 @@ public class Articles {
 		return quantite;
 	}
 	
-	public boolean estDansMagasin(Articles magasin[], int NbArticles, Articles articles) { //Stockage//
-		boolean test = false;
-		for (int i=0;i<NbArticles;i++) {
-			if (magasin[i].getNom()==articles.getNom()) {
-				test=false;
-				return test;
-			}
-		}
-		return test;
+	public float getPrix() {
+		return prix;
 	}
 	
-	public void Maj(Articles magasin[], Articles articles) {
-		int i = 0;
-		while (magasin[i].getNom()!=articles.getNom()) {
-			i += 1;
-		}
-		int quantite = magasin[i].getQuantite() + articles.getQuantite();
-		  
+	public float getPrixAchat() {
+		return prixAchat;
 	}
-
+	
+	public boolean estDansMagasin(Articles tab[], int NbArticles) { //Stockage//
+		for (int i=0;i<NbArticles;i++) {
+			if (tab[i].getNom()==nom) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void remplir(Articles tab[], Articles articles, Articles tab2[]) {
+		if (estDansMagasin(tab, NbArticles)==false) {
+			tab[NbArticles]=articles;
+			NbArticles+=1;
+		}
+		else {
+			int i=0;
+			while (tab[i].getNom()!=articles.getNom()) {
+				i+=1;
+			}
+			quantite += tab[i].getQuantite();
+			tab[i]=articles;
+		}
+		tab2=tab;
+	}
+	
+	public void vider(Articles tab2[], Articles articles) {
+		int i=0;
+		while (tab2[i].getNom()!=articles.getNom()) {
+			i+=1;
+		}
+		quantite += tab2[i].getQuantite();
+		tab[i]=articles;
+		prevention(tab2);
+	}
+	
+	public void prevention(Articles tab[]) {
+		for (int i=0;i<NbArticles;i++) {
+			int quantite_temp = tab[i].getQuantite();
+			if (quantite_temp==0) {
+				System.out.println(tab[i].getNom() + "en rupture de stock");	
+			}
+			if (quantite_temp<25){
+				System.out.println("Il reste seulment " + tab[i].getQuantite() + " de " + tab[i].getNom());
+			}
+		}
+		
+	}
+	private void plusVendu(Articles tab[], Articles tab2[]) {
+		 int max=tab[0].getQuantite() - tab2[0].getQuantite();
+		 int e=0;
+		 for (int i=1;i<NbArticles;i++) {
+			 if (tab[i].getQuantite() - tab2[i].getQuantite()>max) {
+				 max=tab[i].getQuantite() - tab2[i].getQuantite();
+				 e = i;
+			 }
+		 }
+		 System.out.println("L'article le plus vendu est " + tab[e].getNom());
+	 }
+	 
+	 private void moinsVendu(Articles tab[], Articles tab2[]) {
+		 int min=tab[0].getQuantite() - tab2[0].getQuantite();
+		 int e=0;
+		 for (int i=1;i<NbArticles;i++) {
+			 if (tab[i].getQuantite() - tab2[i].getQuantite()<min) {
+				 min=tab[i].getQuantite() - tab2[i].getQuantite();
+				 e=i;
+			 }
+		 }
+		 System.out.println("L'article le moins vendu est " + tab[e].getNom());
+	 }
+	 
+	 private void plusRentable(Articles tab[], Articles tab2[]) {
+		 float max=0;
+		 int e =0;
+		 for (int i=0;i<NbArticles;i++) {
+			 int nb_vente = tab[i].getQuantite() - tab2[i].getQuantite();
+			 if (max<nb_vente*tab[i].getPrix() - tab[i].getQuantite()*tab[i].getPrixAchat()) {
+				 max = nb_vente * tab[i].getPrix();
+				 e = i;
+			 }
+		 }
+		 System.out.println("L'article le plus rentable est " + tab[e].getNom());
+	 }
+	 
+	 private void moinsRentable(Articles tab[], Articles tab2[]) {
+		 float min=0;
+		 int e =0;
+		 for (int i=0;i<NbArticles;i++) {
+			 int nb_vente = tab[i].getQuantite() - tab2[i].getQuantite();
+			 if (min>nb_vente*tab[i].getPrix() - tab[i].getQuantite()*tab[i].getPrixAchat()) {
+				 min = nb_vente * tab[i].getPrix();
+				 e = i;
+			 }
+		 }
+		 System.out.println("L'article le moins rentable est " + tab[e].getNom());
+	 }
+	 
+	 public void comparer(Articles tab[], Articles tab2[]) {
+		 plusVendu(tab, tab2);
+		 moinsVendu(tab, tab2);
+		 plusRentable(tab, tab2);
+		 moinsRentable(tab, tab2);
+	 }
+	 
 }
